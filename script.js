d28a4d0fff3b5002cb751b4f69a04cfd3077dea6
georgiaevxns1 <georgiaevxns@outlook.com>
2026-02-01 15:17:51 +0100

    Add code to allow user to change the grid size

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('.grid');
 
+
 for (let i = 1; i <=256; i++) {
     const gridSquare = document.createElement("div");
     gridSquare.classList.add('gridSquare');
@@ -10,3 +11,30 @@ for (let i = 1; i <=256; i++) {
     });
     grid.appendChild(gridSquare);
 }
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const gridSize = input.valueAsNumber;
+    if (Number.isNaN(gridSize) || gridSize > 100) {
+        return;
+    } else {
+       document.querySelectorAll('.gridSquare').forEach(square => square.remove());
+       
+       for (let i = 1; i <= (gridSize * gridSize); i++) {
+            const customGrid = document.createElement("div");
+            customGrid.classList.add('gridSquare');
+            customGrid.style.width = (grid.clientWidth / gridSize) - 2 + 'px';
+            customGrid.style.height = (grid.clientWidth / gridSize) - 2 + 'px';
+            customGrid.addEventListener('mouseover', function () {
+                customGrid.style.backgroundColor = 'lightBlue';
+        });
+        grid.appendChild(customGrid);
+       } 
+    }
+});
+    
+
+    
